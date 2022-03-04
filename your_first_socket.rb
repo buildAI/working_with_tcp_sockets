7d1726cd167a5d9e4555f2068d2b9f810a060490
Karthik Yelchuru <yelkarthik@gmail.com>
2022-03-04 20:44:54 +0530

    Use localhost and bind to 4481. Showcase 0.0.0.0 and 1.2.3.4. Comment it out out because it errors out

--- your_first_socket.rb
+++ your_first_socket.rb
@@ -1,7 +1,13 @@
 require 'socket'
 
-socket = Socket.new(:INET, :STREAM)
+local_socket = Socket.new(:INET, :STREAM)
+local_addr = Socket.pack_sockaddr_in(4481, '127.0.0.1')
+local_socket.bind(local_addr)
 
-addr = Socket.pack_sockaddr_in(4481, '0.0.0.0')
+# any_socket = Socket.new(:INET, :STREAM)
+# any_addr = Socket.pack_sockaddr_in(4481, '0.0.0.0')
+# any_socket.bind(any_addr)
 
-socket.bind(addr)
+# error_socket = Socket.new(:INET, :STREAM)
+# error_addr = Socket.pack_sockaddr_in(4481, '1.2.3.4')
+# error_socket.bind(error_addr)
